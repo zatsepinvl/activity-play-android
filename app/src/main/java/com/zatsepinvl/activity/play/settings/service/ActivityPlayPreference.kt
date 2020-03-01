8836aa6b27f12f86e8c316d18ee528b613acc804
Vladimir Zatsepin <zatsepin.vl@gmail.com>
2020-03-01 18:52:36 +0300

    Add using default language

--- app/src/main/java/com/zatsepinvl/activity/play/settings/service/ActivityPlayPreference.kt
+++ app/src/main/java/com/zatsepinvl/activity/play/settings/service/ActivityPlayPreference.kt
@@ -2,7 +2,10 @@ package com.zatsepinvl.activity.play.settings.service
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.content.res.Resources
+import androidx.core.os.ConfigurationCompat
 import androidx.preference.PreferenceManager
+import com.zatsepinvl.activity.play.dictionary.SupportedLanguage
 import com.zatsepinvl.activity.play.dictionary.getSupportedLanguageFromTag
 import com.zatsepinvl.activity.play.settings.model.ActivityPlayPreferenceActionKey
 import com.zatsepinvl.activity.play.settings.model.ActivityPlayPreferenceKey
@@ -11,10 +14,16 @@ import com.zatsepinvl.activity.play.settings.model.ActivityPlayPreferences
 object ActivityPlayPreference {
 
     fun getActivityPlayPreferences(context: Context): ActivityPlayPreferences {
+        val currentLanguageTag = getDefaultLanguageTag()
+        val defaultLanguage = SupportedLanguage.values()
+            .find { it.tag == currentLanguageTag } ?: SupportedLanguage.EN
         return PreferenceManager.getDefaultSharedPreferences(context).run {
             ActivityPlayPreferences(
                 dictionaryLanguage = getSupportedLanguageFromTag(
-                    getString(ActivityPlayPreferenceKey.DICTIONARY_LANGUAGE.key, "en")!!
+                    getString(
+                        ActivityPlayPreferenceKey.DICTIONARY_LANGUAGE.key,
+                        defaultLanguage.tag
+                    )!!
                 ),
                 fineForSkipping = getBoolean(
                     ActivityPlayPreferenceKey.FINE_FOR_SKIPPING.key,
@@ -27,6 +36,10 @@ object ActivityPlayPreference {
                 ).map { it.action }.toSet()
             )
         }
+    }
+
+    private fun getDefaultLanguageTag(): String {
+        return ConfigurationCompat.getLocales(Resources.getSystem().configuration)[0].language
     }
 
     fun getSharedPreferences(context: Context): SharedPreferences {
